eda825de7ff88eee0ee7b11027a9b6626197634a
Rafael Lopes Francisco <rafaellopesfrancisco@iMac-de-Rafael.lan>
2015-05-12 17:04:12 +0100

    Commit inicial

---
+++ Trabalho de SO Servidor/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Trabalho de SO Servidor
+//
+//  Created by Rafael Lopes Francisco on 12/05/15.
+//  Copyright (c) 2015 Rafael Lopes Francisco. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
