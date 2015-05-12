be8dda35e0aa4ea0a7aaec9dd161b27a8857b6ed
Rafael Lopes Francisco <rafaellopesfrancisco@iMac-de-Rafael.lan>
2015-05-12 17:34:43 +0100

    Declaração básica de structs

--- Trabalho de SO/jogoPrincipal.h
+++ Trabalho de SO/jogoPrincipal.h
@@ -8,6 +8,19 @@
 
 #ifndef Trabalho_de_SO_jogoPrincipal_h
 #define Trabalho_de_SO_jogoPrincipal_h
+#define NUM_TAGS 10
+
+typedef struct sala{
+    int ID;
+    char* desc;
+    char* tagsEfeito[NUM_TAGS];
+    struct sala *next;
+}sala;
+
+typedef struct dungeon{
+    int num_salas;
+    sala salas[];
+}dungeon;
 
 void comecarJogo();
 
---
+++ Trabalho de SO/utils.h
@@ -0,0 +1,14 @@
+//
+//  utils.h
+//  Trabalho de SO
+//
+//  Created by Rafael Lopes Francisco on 12/05/15.
+//  Copyright (c) 2015 Rafael Lopes Francisco. All rights reserved.
+//
+
+#ifndef __Trabalho_de_SO__utils__
+#define __Trabalho_de_SO__utils__
+
+#include <stdio.h>
+
+#endif /* defined(__Trabalho_de_SO__utils__) */
