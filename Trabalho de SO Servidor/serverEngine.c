670ef60741c1f334200367b98c594ca0ebd984c3
Rafael Lopes Francisco <rafaellopesfrancisco@iMac-de-Rafael.lan>
2015-06-07 15:49:17 +0100

    Corrijido problema de threading, com fork()

--- Trabalho de SO Servidor/serverEngine.c
+++ Trabalho de SO Servidor/serverEngine.c
@@ -20,34 +20,41 @@ void startServer(){
     int server_to_client;
     char *myfifo2 = "/tmp/server_to_client_fifo";
     
+    unlink(myfifo);
+    unlink(myfifo2);
+    
     /* create the FIFO (named pipe) */
     mkfifo(myfifo, 0666);
     mkfifo(myfifo2, 0666);
+    printf("FIFO's criados\n");
     
+    //Threading - inutilizado por agora
     fork();
     pid_t child_ppid = getppid(); //get the child's parent pid
     
     if (child_ppid == pid_server) //if the current process is a child of the main process
     {
-        server_to_client = open(myfifo2, O_WRONLY);
-        write(server_to_client, "Server ligado", sizeof("Server ligado"));
-        printf("FIFO de saida criado\n");
-        close(server_to_client);
+        
         exit(1); //making sure to avoid fork bomb
     }
+    
+    server_to_client = open(myfifo2, O_WRONLY);
+    write(server_to_client, "Server ligado", sizeof("Server ligado"));
+    printf("FIFO de saida criado\n");
+    close(server_to_client);
 
     /* open, read, and display the message from the FIFO */
     client_to_server = open(myfifo, O_RDONLY);
     printf("FIFO's criados\n");
     
     do{
-        pid_t *pid_temp;
+        pid_t pid_temp;
         dungeon *temp;
         
-        read(client_to_server, pid_temp, sizeof(pid_t));
+        read(client_to_server, &pid_temp, sizeof(pid_t));
         read(client_to_server, temp, sizeof(dungeon));
        
-        addCliente(*head, *pid_temp, temp);
+        addCliente(*head, pid_temp, temp);
     }while(starting);
 }
 
--- Trabalho de SO/correrJogo.h
+++ Trabalho de SO/correrJogo.h
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <string.h>
 
 void jogoNormal();
